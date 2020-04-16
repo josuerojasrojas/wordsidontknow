e4a468925f96fa9629aa292be5057e1047e02dd0
Paul Barron <p.barron@outlook.com>
2020-04-15 21:33:59 -0500

    Added proptypes to TextInput component.

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import styles from "./styles.module.css";
+import PropTypes from "prop-types";
 
 const TextInput = ({
   className,
@@ -26,6 +27,14 @@ const TextInput = ({
 
 TextInput.defaultProps = {
   type: "text",
+};
+
+TextInput.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+  value: PropTypes.string,
 };
 
 export default TextInput;
