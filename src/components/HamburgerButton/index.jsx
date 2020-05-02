44cf7588177bc872df65742ff8baffbe3f3f88c5
Paul Barron <p.barron@outlook.com>
2020-05-01 19:57:42 -0500

    Hamburger well done.

---
+++ src/components/HamburgerButton/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "./styles.module.css";
+
+const HamburgerButton = ({ toggle, onClick }) => {
+  const [isRender, setIsRender] = useState(false);
+
+  const _onClick = () => {
+    setIsRender(true);
+    onClick();
+  };
+
+  return (
+    <>
+      <div
+        onClick={_onClick}
+        className={classNames(styles.hamburgerButton, {
+          [styles.isToggled]: isRender && toggle,
+          [styles.isNotToggled]: isRender && !toggle,
+        })}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
+};
+
+HamburgerButton.propTypes = {
+  toggle: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+export default HamburgerButton;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import Logo from "components/Logo";
 import logoSrc from "assets/circle.svg";
 import { Link } from "react-router-dom";
+import HamburgerButton from "components/HamburgerButton";
 
 const Header = ({ className }) => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className={classNames(className, styles.header)}>
       <Link to="/">
         <Logo imageSrc={logoSrc} />
       </Link>
+      <HamburgerButton
+        toggle={toggle}
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      />
     </div>
   );
 };
